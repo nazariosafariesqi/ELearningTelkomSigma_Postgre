c595013d5f5fe5e0fa5e6a902d9932834651ec79
Nazario Safariesqi <nazariotyo@gmail.com>
2022-10-05 20:27:52 +0700

    First Commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'ELearning';
+$CFG->dbuser    = 'ELearning_User';
+$CFG->dbpass    = '123';
+$CFG->prefix    = 'elr_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 5432,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://localhost/ELearning';
+$CFG->dataroot  = 'H:\\Semua File\\XAMPP\\moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
